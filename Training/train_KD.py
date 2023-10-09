4f8ea1623d62bcfbe0807f3bcf7488b033594561
Benedetto Simone <sim.benede@gmail.com>
2023-10-09 16:25:51 +0200

    optimize code with functions in train_KD.py

--- Training/train_KD.py
+++ Training/train_KD.py
@@ -31,7 +31,40 @@ PREDICT_DICTIONARY = os.path.join(CURRENT_PATH, 'dictionaries', 'phrases.txt')
 def curriculum_rules(epoch):
     return {'sentence_length': -1, 'flip_probability': 0.5, 'jitter_probability': 0.05}
 
+def compute_ensemble_output(student_predictions, student_weights):
+    ensemble_output = []
 
+    # Get how many predictions
+    for i in range(student_predictions[0][1].shape[0]):
+        weighted_predictions = []
+        for student_idx, pred in student_predictions:
+            # Multiply each tensor for the student weight
+            weighted_predictions.append(pred[i] * student_weights[student_idx])
+
+        # For each sample (i.e. pred1 of s1, pred1 of s2, pred1 of s3)
+        # pred * w1 + pred * w2 + pred * w3
+        # Sum each student tensor
+        ensemble_prediction = np.sum(weighted_predictions, axis=0)
+        ensemble_output.append(ensemble_prediction)
+
+    ensemble_output = np.array(ensemble_output)
+
+    return ensemble_output
+
+
+def compute_ensemble_mean_loss(ensemble_output, x_train):
+
+    labels = tf.convert_to_tensor(x_train['the_labels'])
+    label_length = tf.convert_to_tensor(x_train['label_length'].reshape(-1, 1))
+    input_length = tf.convert_to_tensor(x_train['input_length'].reshape(-1, 1))
+    ensemble_output = tf.convert_to_tensor(ensemble_output)
+    ensemble_ctc_loss = ctc_lambda_func([ensemble_output, labels, input_length, label_length])
+    # Todo check sum of probabilities
+
+    # Compute the mean CTC loss
+    ensemble_mean_loss = tf.reduce_mean(ensemble_ctc_loss, axis=0)
+
+    return ensemble_mean_loss
 
 def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, absolute_max_string_len, minibatch_size,
           num_samples_stats, peer_networks_n):
@@ -50,8 +83,6 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
         lipnet = LipNet(img_c=img_c, img_w=img_w, img_h=img_h, frames_n=frames_n,
                         absolute_max_string_len=absolute_max_string_len, output_size=lip_gen.get_output_size())
         lipnet.summary()
-
-        #adam = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
         # load weights
         if start_epoch == 0:
@@ -75,7 +106,6 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
         print("Epoch {}/{}".format(epoch + 1, stop_epoch))
 
         # For each batch we train simultaneously n students
-
         b = 0
         student_weights = []
         student_predictions = []
@@ -89,7 +119,6 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
 
             # train all students on the same batch
             for n, lipnet in enumerate(peer_networks_list):
-                print("Student: {}".format(n))
 
                 # define callbacks
                 statistics = Statistics(lipnet, lip_gen.next_val(), decoder, num_samples_stats,
@@ -97,20 +126,30 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
                 visualize = Visualize(os.path.join(OUTPUT_DIR, run_name), lipnet, lip_gen.next_val(), decoder,
                                       num_display_sentences=minibatch_size)
 
+                '''
+                ########################################################################################
+                ### STEP 1: train students on the batch
+                ########################################################################################
+                '''
+
                 with tf.GradientTape() as tape:
 
                     # Forward pass
                     # this result is a set of prob + losses
                     y_pred = lipnet.model(x_train, training=True)
 
-                    print("Loss: {}".format(y_pred[1]))
-
                     # Compute the mean CTC loss
                     student_ctc_loss = tf.reduce_mean(y_pred[1], axis=0)
+                    print("Student {} mean loss: {}".format(n, student_ctc_loss))
 
                 student_predictions.append((n, y_pred[0]))
                 student_losses.append(student_ctc_loss)
 
+                '''
+                ########################################################################################
+                ### STEP 2: compute student weights to aggregate predictions
+                ########################################################################################
+                '''
                 # make prediction on current batch, decode results and save them to compute
                 # metrics useful to perform KD attention
 
@@ -118,52 +157,38 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
                 #y_pred, mean_bleu, mean_bleu_norm = statistics.on_batch_end(x_train)
                 # TODO check normalization of weights
                 # student_weights.append(mean_bleu_norm)
-                student_weights = []
-                student_weights.append(0.4)
-                student_weights.append(0.6)
+                student_weights = [0.4, 0.6]
 
+            '''
+            ########################################################################################
+            ### STEP 3: compute ensemble output
+            ########################################################################################
+            '''
             # At the end of each batch compute ensemble output
-            ensemble_predictions = []
+            ensemble_output = compute_ensemble_output(student_predictions, student_weights)
 
-            # Get how many predictions
-            for i in range(student_predictions[0][1].shape[0]):
-                weighted_predictions = []
-                for student_idx, pred in student_predictions:
-
-                    # Multiply each tensor for the student weight
-                    weighted_predictions.append(pred[i] * student_weights[student_idx])
-
-                # For each sample (i.e. pred1 of s1, pred1 of s2, pred1 of s3)
-                # pred * w1 + pred * w2 + pred * w3
-                # Sum each student tensor
-                ensemble_prediction = np.sum(weighted_predictions, axis=0)
-                ensemble_predictions.append(ensemble_prediction)
-
-            ensemble_predictions = np.array(ensemble_predictions)
-
-
-            # TODO Build multi-loss function
-
+            '''
+            ########################################################################################
+            ### STEP 4: compute multi-loss function
+            ########################################################################################
+            '''
             # L = CTC loss ensemble predictions and truth + sum(1,N)[(CTC loss student predictions(i) and truth)+
             # (LDK divergence students predictions(i) and ensemble predictions)
 
-            # CTC loss between ensemble predictions and truth
-            labels = tf.convert_to_tensor(x_train['the_labels'])
-            label_length = tf.convert_to_tensor(x_train['label_length'].reshape(-1, 1))
-            input_length = tf.convert_to_tensor(x_train['input_length'].reshape(-1, 1))
-            ensemble_predictions = tf.convert_to_tensor(ensemble_predictions)
-            ensemble_ctc_loss = ctc_lambda_func([ensemble_predictions, labels, input_length, label_length])
-
-            # Todo check sum of probabilities
-
-            # Calculate the mean CTC loss
-            mean_ctc_loss = tf.reduce_mean(y_pred[1], axis=0)
+            # Ensemble CTC loss
+            ensemble_mean_loss = compute_ensemble_mean_loss(ensemble_output, x_train)
+            print("Ensemble mean loss: {}".format(ensemble_mean_loss))
 
             # CTC loss between students predictions and truth
             # Already in student_losses array
 
             # LDK divergence between students predictions and ensemble predictions
 
+            '''
+            ########################################################################################
+            ### STEP 5: optimize students
+            ########################################################################################
+            '''
             #grads = tape.gradient(mean_ctc_loss, lipnet.model.trainable_variables)
             #adam.apply_gradients(zip(grads, lipnet.model.trainable_variables))
 
