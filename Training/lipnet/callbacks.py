fix kd using logits and softmax temperature.

Training/lipnet/callbacks.py
@@ -51,8 +51,9 @@ class Statistics(tf.keras.callbacks.Callback):
             for key, value in output_batch.items():
                 sub_batch[key] = value[0:num_proc]
 
-            # [0] because the output of the model is: predictions + losses
-            y_pred = self.model_container.model(sub_batch, training=False)[0]
+            # [0] because the output of the model is: logits + losses
+            logits = self.model_container.model(sub_batch, training=False)[0]
+            y_pred = tf.nn.softmax(logits)
 
             input_length = sub_batch['input_length']
             decoded_res = self.decoder.decode(y_pred, input_length)
@@ -128,6 +129,7 @@ class Statistics(tf.keras.callbacks.Callback):
                  "Mean BLEU (Norm)"])
 
 
+
     def on_batch_end(self, y_pred, batch, logs=None):
         """
         Callback called at the end of each batch during model training to save batch results for KD attention.
@@ -201,8 +203,10 @@ class Visualize(tf.keras.callbacks.Callback):
         for key, value in output_batch.items():
             display_sentences[key] = value[0:self.num_display_sentences]
 
-        # [0] because the output of the model is: predictions + losses
-        y_pred = self.model_container.model(display_sentences, training=False)[0]
+        # [0] because the output of the model is: logits + losses
+        logits = self.model_container.model(display_sentences, training=False)[0]
+        y_pred = tf.nn.softmax(logits)
+
         input_length = display_sentences['input_length']
         res = self.decoder.decode(y_pred, input_length)
 

Training/lipnet/model.py
@@ -64,7 +64,7 @@ class LipNet(object):
         self.loss_out = CTC('ctc', [self.y_pred, self.labels, self.input_length, self.label_length])
 
         self.model = tf.keras.Model(inputs=[self.input_data, self.labels, self.input_length, self.label_length],
-                                    outputs=[self.y_pred, self.loss_out])
+                                    outputs=[self.dense1, self.loss_out])
 
     def summary(self):
         self.model.summary()

Training/lipnet/online_kd.py
@@ -4,7 +4,11 @@ import numpy as np
 from layers import ctc_lambda_func
 import matplotlib.pyplot as plt
 
-def extract_features(statistics, y_pred, x_train, f1_array, f2_array, f3_array):
+
+def extract_features(statistics, logits, x_train, f1_array, f2_array, f3_array):
+
+
+    y_pred = tf.nn.softmax(logits)
 
     # Extract features
     f1, f2, f3 = statistics.on_batch_end(y_pred, x_train)
@@ -54,27 +58,27 @@ def ensembling_strategy(f1, f2, f3, peer_networks_n):
     return student_weights
 
 
-def compute_ensemble_output(student_predictions, student_weights):
+def compute_ensemble_output(student_logits, student_weights):
     ensemble_output = []
 
-    # Get how many predictions
-    for i in range(student_predictions[0][1].shape[0]):
-        #Get weighted prediction of sample
-        weighted_predictions = []
-        weights_pred_i = []
+    # Get how many samples
+    for i in range(student_logits[0][1].shape[0]):
+        # Get weighted logits of sample
+        weighted_logits = []
+        weights_log_i = []
 
-        #Get i prediction of each student
-        for student_idx, pred in student_predictions:
-            # Multiply each tensor for the student weight for the corresponding prediction
-            weighted_predictions.append(pred[i] * student_weights[student_idx][i])
-            weights_pred_i.append(student_weights[student_idx][i])
+        # Get i logits of each student
+        for student_idx, logits in student_logits:
+            # Multiply each tensor for the student weight for the corresponding logits
+            weighted_logits.append(logits[i] * student_weights[student_idx][i])
+            weights_log_i.append(student_weights[student_idx][i])
 
         # For each sample (i.e. pred1 of s1, pred1 of s2, pred1 of s3)
         # (pred * w1 + pred * w2 + pred * w3) / w1+w2+w3
         # Sum each student tensor
-        ensemble_prediction = np.sum(weighted_predictions, axis=0)
-        ensemble_prediction = ensemble_prediction / np.sum(weights_pred_i)
-        ensemble_output.append(ensemble_prediction)
+        ensemble_logits = np.sum(weighted_logits, axis=0)
+        ensemble_logits = ensemble_logits / np.sum(weights_log_i)
+        ensemble_output.append(ensemble_logits)
 
     ensemble_output = np.array(ensemble_output)
 
@@ -87,6 +91,8 @@ def compute_ensemble_loss(ensemble_output, x_train):
     label_length = tf.convert_to_tensor(x_train['label_length'].reshape(-1, 1))
     input_length = tf.convert_to_tensor(x_train['input_length'].reshape(-1, 1))
     ensemble_output = tf.convert_to_tensor(ensemble_output)
+
+    ensemble_output = tf.nn.softmax(ensemble_output)
     ensemble_ctc_loss = ctc_lambda_func([ensemble_output, labels, input_length, label_length])
 
     # Compute the mean CTC loss
@@ -109,16 +115,20 @@ def kl_divergence(student_prediction, ensemble_output):
     return sequence_kl_divergence, mean_kl_divergence
 
 
-def kd_loss(student_predictions, ensemble_output, temperature):
+def kd_loss(student_logits, ensemble_output, temperature):
 
     students_kl = []
 
-    # Get i prediction of each student and compute kl divergence
-    for student_idx, pred in student_predictions:
+    # Get i logits of each student and compute kl divergence
+    for student_idx, logits in student_logits:
         kl_values_batch = []
 
-        for i in range(pred.shape[0]):
-            kl_values, mean_kl = kl_divergence(pred[i], ensemble_output[i])
+        for i in range(logits.shape[0]):
+
+            logits_i =  tf.nn.softmax(tf.math.divide(logits[i], temperature))
+            ensemble_output_i = tf.nn.softmax(tf.math.divide(ensemble_output[i], temperature))
+
+            kl_values, mean_kl = kl_divergence(logits_i, ensemble_output_i)
             kl_values_batch.append(mean_kl)
             # Use to show scatter plot
             # kl_values_batch.append(kl_values)
@@ -143,13 +153,13 @@ def kd_loss(student_predictions, ensemble_output, temperature):
     return students_kl
 
 
-def multiloss_function(peer_networks_n, ensemble_output, x_train, student_predictions, temperature, student_losses, distillation_strength):
+def multiloss_function(peer_networks_n, ensemble_output, x_train, student_logits, temperature, student_losses, distillation_strength):
 
     # Ensemble CTC loss
     ensemble_loss = compute_ensemble_loss(ensemble_output, x_train)
     print("Ensemble mean loss: {}".format(ensemble_loss))
 
-    students_kl = kd_loss(student_predictions, ensemble_output, temperature)
+    students_kl = kd_loss(student_logits, ensemble_output, temperature)
 
     student_losses_sum = 0
     for s in range(peer_networks_n):

Training/train_KD.py
@@ -21,6 +21,7 @@ import tensorflow as tf
 from lipnet.online_kd import extract_features, ensembling_strategy, compute_ensemble_output, multiloss_function
 
 
+
 np.random.seed(55)
 
 DATASET_DIR = os.path.join(CURRENT_PATH, 'datasets')
@@ -36,7 +37,7 @@ def curriculum_rules(epoch):
     return {'sentence_length': -1, 'flip_probability': 0.5, 'jitter_probability': 0.05}
 
 def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, absolute_max_string_len, minibatch_size,
-          num_samples_stats, peer_networks_n):
+          num_samples_stats, peer_networks_n, distillation_strength, temperature):
     curriculum = Curriculum(curriculum_rules)
     lip_gen = BasicGenerator(dataset_path=DATASET_DIR,
                              minibatch_size=minibatch_size,
@@ -94,7 +95,7 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
 
         print("Epoch {}/{}".format(epoch + 1, stop_epoch))
 
-        student_predictions = []
+        student_logits = []
         student_losses = []
 
         # For each batch train simultaneously n students
@@ -115,29 +116,30 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
                     statistics = callback_list[n][0]
 
                     # Forward pass
-                    # this result is a set of prob + losses
-                    y_pred = lipnet.model(x_train, training=True)
+                    # this result is a set of logits + losses
+                    model_out = lipnet.model(x_train, training=True)
+
+                    logits = model_out[0]
+                    losses = model_out[1]
 
                     # Compute the mean CTC loss
-                    student_ctc_loss = tf.reduce_mean(y_pred[1], axis=0)
+                    student_ctc_loss = tf.reduce_mean(losses, axis=0)
                     print("Student {} mean loss: {}".format(n, student_ctc_loss))
 
-                    student_predictions.append((n, y_pred[0]))
+                    student_logits.append((n, logits))
                     student_losses.append(student_ctc_loss)
 
                     # Extract_features
-                    f1_array, f2_array, f3_array = extract_features(statistics,y_pred[0], x_train, f1_array, f2_array, f3_array)
+                    f1_array, f2_array, f3_array = extract_features(statistics, logits, x_train, f1_array, f2_array, f3_array)
 
                 # At the end of each batch compute ensemble weights
                 student_weights = ensembling_strategy(f1_array, f2_array, f3_array, peer_networks_n)
     
-                # Sum weighted predictions to compute ensemble output
-                # TODO use logits
-                ensemble_output = compute_ensemble_output(student_predictions, student_weights)
+                # Sum weighted logits to compute ensemble output
+                ensemble_output = compute_ensemble_output(student_logits, student_weights)
 
-                # TODO set temperature and distillation_strength
-                ensemble_loss, multiloss_value = multiloss_function(peer_networks_n, ensemble_output, x_train, student_predictions, 1,
-                                       student_losses, 1)
+                ensemble_loss, multiloss_value = multiloss_function(peer_networks_n, ensemble_output, x_train, student_logits, temperature,
+                                       student_losses, distillation_strength)
 
                 print("Multiloss value:  {}".format(multiloss_value))
 
@@ -179,15 +181,26 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
 
 if __name__ == '__main__':
     run_name = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    # 1st parameter - run_name
-    # 2nd parameter - start_epoch
-    # 3rd parameter - stop_epoch
-    # 4th parameter - img_c (num of channel)
-    # 5th parameter - img_w
-    # 6th parameter - img_h
-    # 7th parameter - frames_n
-    # 8th parameter - absolute_max_string_length (max len of sentences)
-    # 9th parameter - minibatch_size
-    # 10th parameter - num_samples_stats (number of samples for statistics evaluation at each epoch)
-    # 11th parameter - number of peer network
-    train(run_name, 0, 10, 3, 100, 50, 100, 54, 19, 95, 2)
+
+    start_epoch = 0
+    stop_epoch = 10
+
+    # Samples properties
+    img_c = 3  # Image channels
+    img_w = 100  # Image width
+    img_h = 50  # Image height
+    frames_n = 100  # Number of frames per video
+
+    absolute_max_string_len = 54  # Max sentence length
+
+    minibatch_size = 19  # Minibatch size
+
+    num_samples_stats = 95  # Number of samples for statistics evaluation per epoch
+
+    # KD parameters
+    peer_networks_n = 2  # Number of peer networks
+    distillation_strength = 1  # Distillation strength
+    temperature = 3  # Softmax's temperature applied to logits
+
+    train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, absolute_max_string_len, minibatch_size,
+          num_samples_stats, peer_networks_n, distillation_strength, temperature)