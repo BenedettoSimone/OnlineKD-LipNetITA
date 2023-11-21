fix plot metrics to show cer related to min wer. updated plots. add kl divergences plots. add LipNet-256.

Training/lipnet/callbacks.py
@@ -73,7 +73,6 @@ class Statistics(tf.keras.callbacks.Callback):
         mean_bleu, mean_bleu_norm = self.get_mean_bleu_score(data)
 
         # Compute the mean CTC loss
-        # TODO check mean
         validation_loss = np.mean(val_ctc_losses)
 
         return {
@@ -137,7 +136,7 @@ class Statistics(tf.keras.callbacks.Callback):
                  "Mean BLEU (Norm)"])
 
 
-    '''
+
     def on_batch_end(self, y_pred, batch, logs=None):
         """
         Callback called at the end of each batch during model training to save batch results for KD attention.
@@ -158,7 +157,7 @@ class Statistics(tf.keras.callbacks.Callback):
         bleu_scores = self.get_bleu_scores(batch_results)
 
         return cer_values, wer_values, bleu_scores
-    '''
+
 
 
     def on_epoch_end(self, epoch, logs={}):

Training/train_KD.py
@@ -55,9 +55,9 @@ def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, abso
             start_file_w = os.path.join(OUTPUT_DIR, 'startWeight/unseen-weights178.h5')
             lipnet.model.load_weights(start_file_w)
 
-        # load preexisting trained weights for the model
+        # load preexisting trained weights for the model weights113_peer_01.h5
         if start_epoch > 0:
-            weight_file = os.path.join(OUTPUT_DIR, os.path.join(run_name, 'weights%02d.h5' % (start_epoch - 1)))
+            weight_file = os.path.join(OUTPUT_DIR, os.path.join(f"2023_10_31_17_06_50/s{n}", f'weights%02d_peer_0{n}.h5' % (start_epoch - 1)))
             lipnet.model.load_weights(weight_file)
 
         adam = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
@@ -220,7 +220,7 @@ if __name__ == '__main__':
     run_name = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 
     start_epoch = 0
-    stop_epoch = 100
+    stop_epoch = 340
 
     # Samples properties
     img_c = 3  # Image channels
@@ -230,7 +230,7 @@ if __name__ == '__main__':
 
     absolute_max_string_len = 54  # Max sentence length
 
-    minibatch_size = 32  # Minibatch size
+    minibatch_size = 16  # Minibatch size
 
     num_samples_stats = 95  # Number of samples for statistics evaluation per epoch
 